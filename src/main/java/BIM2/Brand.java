baa7f5932ce9c5e3a3580559071273113243a993
Jakob Ladegaard Møller <jlm1999@live.dk>
2022-02-22 10:49:30 +0100

    Create Brand.java

---
+++ src/main/java/BIM2/Brand.java
@@ -0,0 +1,12 @@
+package BIM2;
+
+import java.util.Date;
+import java.util.List;
+
+public class Brand {
+    String name;
+    String description;
+    Date founded;
+    String headquarters;
+    List<BrandProduct> products;
+}
