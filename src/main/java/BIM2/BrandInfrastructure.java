f00b2f71f53c1870a4c07e2381a626936ee6db47
Jakob Ladegaard Møller <jlm1999@live.dk>
2022-02-22 10:48:04 +0100

    Update BrandInfrastructure.java

--- src/main/java/BIM2/BrandInfrastructure.java
+++ src/main/java/BIM2/BrandInfrastructure.java
@@ -3,8 +3,9 @@ package BIM2;
 import java.util.List;
 
 public interface BrandInfrastructure {
-    void indexBrandInfo();
-    String[] tokenization(String delimiter, String ignore);
-
-    List searchBrands();
+    List<Brand> getBrandsFromSearchTokens(List<String> tokens);
+    
+    void indexBrands(List<Brand> brands);
+    
+    void setTokenizationParameters(String delimiter, String ignore);
 }
